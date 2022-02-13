cdc2edbc1df884909c801151e0744dfada8622a8
Darkoson <darkothomas80@gmail.com>
2022-02-13 16:32:40 +0000

    rendering a react project and integration of the generated bundle into the html file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("np");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Movie List</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
